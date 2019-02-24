7428f329bef4b38bbec5fdbe4f998fdd54188eb9
Steven Andrade <stvn.piano@gmail.com>
2019-02-24 18:53:04 -0500

    Saving Data in Mysql

--- src/routes/links.js
+++ src/routes/links.js
@@ -8,8 +8,15 @@ router.get('/add', (req, res)=>{
     res.render('links/add');
 });
 
-router.post('/add', (req,res)=>{
-
+router.post('/add', async (req,res)=>{
+    //console.log(req.body);
+    const {title, url, description} = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?',[newLink]);
     res.send('recibido');
 
 });
