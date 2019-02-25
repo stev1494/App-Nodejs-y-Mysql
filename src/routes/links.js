783367944b56451b78ce42d0ed9e4708fcd6060a
Steven Andrade <stvn.piano@gmail.com>
2019-02-24 20:36:22 -0500

    Retrieving Data from Mysql

--- src/routes/links.js
+++ src/routes/links.js
@@ -17,7 +17,14 @@ router.post('/add', async (req,res)=>{
         description
     };
     await pool.query('INSERT INTO links set ?',[newLink]);
-    res.send('recibido');
+    res.redirect('/links');
+
+});
+
+router.get('/', async(req,res)=>{
+
+    const links = await pool.query('SELECT * FROM links');
+    res.render('links/list', { links });
 
 });
 
